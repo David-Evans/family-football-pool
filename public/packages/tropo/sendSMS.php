6e7107f7d0003ed14244ad1b1c45855b01994881
David Evans <david.evans@infusionsoft.com>
2016-09-05 12:34:27 -0700

    Debugging Tropo

--- public/packages/tropo/sendSMS.php
+++ public/packages/tropo/sendSMS.php
@@ -1,8 +1,14 @@
 <?php 
 
+echo 'inside sendSMS...<br />';
+
 	require 'tropo.class.php';
 
+echo 'post require...<br />';
+
 	$session = new Session(); 
+echo 'got session...<br />';
+
 	$to = "+1".$session->getParameters("numbertodial"); 
 	$msg = $session->getParameters("msg"); 
 	    
