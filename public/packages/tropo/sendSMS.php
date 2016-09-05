5f8f44dae87a58305f3d7f7deaee4d05b2b6d03b
David Evans <david.evans@infusionsoft.com>
2016-09-05 13:31:34 -0700

    Still trying to get it to work :-/

--- public/packages/tropo/sendSMS.php
+++ public/packages/tropo/sendSMS.php
@@ -2,15 +2,9 @@
 
 //use Illuminate\Http\Request;
 
-echo 'inside sendSMS...<br />';
-
 	require 'tropo.class.php';
 
-echo 'post require...<br />';
-
-echo 'got session...<br />';
-
-	$to = isset($_GET['numbertodial']) ? "+1".$session->getParameters("numbertodial") : FALSE;
+	$to = isset($_GET['numbertodial']) ? "+1".$_GET['numbertodial'] : FALSE;
 	$msg = isset($_GET['msg']) ? rawurlencode($_GET['msg']) : FALSE; 
 	    
 	$tropo = new Tropo(); 
@@ -18,6 +12,6 @@ echo 'got session...<br />';
 	$tropo->call($to, array('network'=>'SMS')); 
 	$tropo->say($msg); 
 
-	echo $tropo->RenderJson(); 
+	return $tropo->RenderJson(); 
 
 ?> 
