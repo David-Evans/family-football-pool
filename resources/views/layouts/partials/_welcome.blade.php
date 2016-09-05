f948e4cef39f64a1eaafe510b92b9c75b525da39
David Evans <david.evans@infusionsoft.com>
2016-09-05 16:19:25 -0700

    Home page updates

---
+++ resources/views/layouts/partials/_welcome.blade.php
@@ -0,0 +1,7 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+			<a class="navbar-brand" href="\" title="{{ $appName }}"><img class="header-logo" src="/images/ffp-banner.png" /></a>
+        </div>
+	</nav>
+
