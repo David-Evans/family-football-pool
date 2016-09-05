9206aae166162f89496f0c3508bfdbcf462ffcdc
David Evans <david.evans@infusionsoft.com>
2016-09-05 09:50:38 -0700

    Add base files for Tropo API

---
+++ public/packages/tropo/sendSMS.php
@@ -0,0 +1,16 @@
+<?php 
+
+	require '../tropo.class.php';
+
+	$session = new Session(); 
+	$to = "+1".$session->getParameters("numbertodial"); 
+	$msg = $session->getParameters("msg"); 
+	    
+	$tropo = new Tropo(); 
+	    
+	$tropo->call($to, array('network'=>'SMS')); 
+	$tropo->say($msg); 
+
+	return $tropo->RenderJson(); 
+
+?> 
