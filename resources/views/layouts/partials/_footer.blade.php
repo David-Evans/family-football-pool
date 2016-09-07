d4e20aba0605e4925e368d6120c3ebc7cd2899cd
David Evans <david.evans@infusionsoft.com>
2016-09-06 22:01:05 -0700

    Cleaned up un-necessary code

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -9,12 +9,14 @@
     <!-- Metis Menu Plugin JavaScript -->
     <script src="/bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-<!--     <script src="/bower_components/raphael/raphael-min.js"></script>
-    <script src="/bower_components/morrisjs/morris.min.js"></script>
-    <script src="/js/morris-data.js"></script>
- -->
     <!-- Custom Theme JavaScript -->
     <script src="/js/sb-admin-2.js"></script>
 
-    
+    <script>
+    //animate bootstrap notification boxes
+        window.setTimeout(function () {
+            jQuery(".alert").fadeTo(500, 0).slideUp(1000, function () {
+                jQuery(this).remove();
+            });
+        }, 3000);
+    </script>
