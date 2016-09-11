0847ca462c14d16e7289e0da33a6de82d1c96b22
David Evans <david.evans@infusionsoft.com>
2016-09-11 13:44:53 -0700

    Add account management capability

---
+++ resources/views/pages/account.blade.php
@@ -0,0 +1,32 @@
+@extends("layouts.master")
+
+@section("content")        
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                <i class="fa fa-cog fa-fw"></i> My Account
+            </h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Manage your account</div>
+
+                <div class="panel-body">
+                <h3>Coming Soon...</h3>
+                <p>Got an idea or suggestion?  Please <a href="/chat">lay down some smack</a> about it!</p>
+                <p>Current plans include:</p>
+                <ul>
+                	<li>Change your password</li>
+                	<li>Update your avatar, email, cell number</li>
+                	<li>Change your nickname</li>
+                	<li>Update your message preferences</li>
+                </ul>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
