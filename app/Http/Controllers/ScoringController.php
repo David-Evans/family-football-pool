27053718cd92449f7e739d67d6dcbd7609e51dfe
David Evans <david.evans@infusionsoft.com>
2018-09-09 12:54:33 -0700

    remove auth from scoring update for CRON

--- app/Http/Controllers/ScoringController.php
+++ app/Http/Controllers/ScoringController.php
@@ -21,7 +21,7 @@ class ScoringController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
