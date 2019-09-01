fc49075063b961699831637b79ae89540939e5a0
David Evans <david.evans@bsstech.com>
2019-08-31 18:27:53 -0700

    Test group messaging with Nexmo 1.7

--- app/Http/Controllers/NexmoController.php
+++ app/Http/Controllers/NexmoController.php
@@ -96,7 +96,7 @@ if($smsNumber == '4802053477' || $smsNumber == '4802038364') {
                   $fields_string = '';
                   foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
                   rtrim($fields_string, '&');
-
+dd($fields);
                   $ch = curl_init();
                   curl_setopt($ch,CURLOPT_URL, $url);
                   curl_setopt($ch,CURLOPT_POST, count($fields));
