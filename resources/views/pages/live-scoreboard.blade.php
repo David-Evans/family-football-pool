ae983c251eb79bf2ff8799722ff0c6637be027d1
David Evans <david.evans@bsstech.com>
2020-09-13 21:15:40 -0700

    Continuing development of live scoreboard

--- resources/views/pages/live-scoreboard.blade.php
+++ resources/views/pages/live-scoreboard.blade.php
@@ -8,16 +8,15 @@
 	<div class="col-xs-6 col-md-3">
 		<div class="game-detail">
 			<h4>$game->day_of_week</h4>
-		{{$game->home_team}}
-			<div class="visitor">
-				<div class="col-xs-4"><img src="/images/logos/49ers.png" /></div>
-				<div class="col-xs-4">{{$game->visitor_team_short}}</div>
-				<div class="col-xs-4">{{$game->visitor_score}}</div>
+			<div class="row visitor">
+				<div class="logo col-xs-4"><img src="/images/logos/49ers.png" /></div>
+				<div class="team-name col-xs-4">{{$game->visitor_team_short}}</div>
+				<div class="score col-xs-4">{{$game->visitor_score}}</div>
 			</div>
-			<div class="home">
-				<div class="col-xs-4"><img src="/images/logos/49ers.png" /></div>
-				<div class="col-xs-4">{{$game->home_team_short}}</div>
-				<div class="col-xs-4">{{$game->home_score}}</div>
+			<div class="row home">
+				<div class="logo col-xs-4"><img src="/images/logos/49ers.png" /></div>
+				<div class="team-name col-xs-4">{{$game->home_team_short}}</div>
+				<div class="score col-xs-4">{{$game->home_score}}</div>
 			</div>
 		</div>
 	</div>
