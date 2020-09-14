e31b0c2c833ef2f57358d4e649d7d291f42dd736
David Evans <david.evans@bsstech.com>
2020-09-13 21:11:42 -0700

    Continuing development of live scoreboard

--- resources/views/pages/live-scoreboard.blade.php
+++ resources/views/pages/live-scoreboard.blade.php
@@ -11,13 +11,13 @@
 		{{$game->home_team}}
 			<div class="visitor">
 				<div class="col-xs-4"><img src="/images/logos/49ers.png" /></div>
-				<div class="col-xs-4">$game->visitor_team_short</div>
-				<div class="col-xs-4">$game->visitor_score</div>
+				<div class="col-xs-4">{{$game->visitor_team_short}}</div>
+				<div class="col-xs-4">{{$game->visitor_score}}</div>
 			</div>
 			<div class="home">
 				<div class="col-xs-4"><img src="/images/logos/49ers.png" /></div>
-				<div class="col-xs-4">$game->home_team_short</div>
-				<div class="col-xs-4">$game->home_score</div>
+				<div class="col-xs-4">{{$game->home_team_short}}</div>
+				<div class="col-xs-4">{{$game->home_score}}</div>
 			</div>
 		</div>
 	</div>
